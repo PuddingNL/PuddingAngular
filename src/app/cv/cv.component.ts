11f89def30a2e45c6b414a0ba90d321a0854f05f
Jeroen Bijl <jeroenbijl@me.com>
2018-06-06 09:18:16 +0200

    Bugfix. CV toont nu alle items uit de JSON.

--- src/app/cv/cv.component.ts
+++ src/app/cv/cv.component.ts
@@ -1,39 +1,25 @@
 import { Component, OnInit } from "@angular/core";
-import { HttpClient } from '@angular/common/http';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: "app-cv",
   templateUrl: "./cv.component.html",
   styleUrls: ["./cv.component.scss"]
 })
-
 export class CvComponent implements OnInit {
-  
   private _jsonurl = "/assets/resume.json";
 
-  company: string = "";
-  title: string = "";
-  date: string = "";
-  description: string = "";
-  count: number;
+  items = [];
 
   constructor(private http: HttpClient) {}
 
-  ngOnInit() { 
-    this.http.get(this._jsonurl)
-    .subscribe(
-      (data: any[]) => {
-        if(data.length) {
-          for(let i = 0; i < data.length; i++) {
-            this.company = data[i].company;
-            this.title = data[i].title;
-            this.date = data[i].date;
-            this.description = data[i].description;
-            this.count = i;
-          }
+  ngOnInit() {
+    this.http.get(this._jsonurl).subscribe((data: any[]) => {
+      if (data.length) {
+        for (let i = 0; i < data.length; i++) {
+          this.items = data;
         }
       }
-    );
-   }
-
+    });
+  }
 }
