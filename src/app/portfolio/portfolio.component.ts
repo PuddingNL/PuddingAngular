95dab0fa88834c6e6b9e7c1ef44a21203def3ced
Jeroen Bijl <Jeroen.Bijl@clockwork.nl>
2018-08-17 13:53:59 +0200

    Test to see if it works.

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 
+// Compontent.
 @Component({
   selector: "app-portfolio",
   templateUrl: "./portfolio.component.html",
